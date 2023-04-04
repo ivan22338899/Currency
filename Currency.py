640fbbf3d590d4aec082d7daecf114e3a124be20
Ivan Yaiko <jajko.ivan@gmail.com>
2023-04-04 21:07:23 +0100

    Create Currency.py

---
+++ Currency.py
@@ -0,0 +1,14 @@
+#import csv
+import pandas as pd
+df = pd.read_csv(r'Currencies.csv')
+# with open('Currencies.csv') as df:
+    #reader = csv.DictReader(df)
+    #for row in reader:
+        #print(row)
+Total = df['Price'].sum()    
+n = len(df)
+Total2 = Total/n
+print(df.nsmallest(1, ['Price']))
+print(df.nlargest(1, ['Price']))
+print (Total2)
+print (n)
